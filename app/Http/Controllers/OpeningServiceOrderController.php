222282e9e19ff571f1131ca84899a7e6a9df6eb5
chpresidencialpalmares <chpresidencialpalmares@gmail.com>
2025-03-24 09:14:45 -0300

    codigo os completo

--- app/Http/Controllers/OpeningServiceOrderController.php
+++ app/Http/Controllers/OpeningServiceOrderController.php
@@ -2,12 +2,16 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Controllers\Controller;
-
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Validator;
+
 
 use App\Models\OpeningServiceOrder;
 use App\Models\Unit;
+
+//use App\Http\Controllers\Controller;
+
 
 
 class OpeningServiceOrderController extends Controller
@@ -43,7 +47,62 @@ class OpeningServiceOrderController extends Controller
             $array['error'] = 'A propriedade é necessária';
         }
         return $array;
-
-        
     }
+
+    public function addOpeningServiceOrderFile(Request $request) {
+        $array = ['error' => ''];
+        $validator = Validator::make($request->all(), [
+            'photo' => 'required|file|mimes:jpg,png'
+        ]);
+        if (!$validator->fails()) {
+            $file = $request->file('photo')->store('public');
+            $file = explode('public/', $file);
+            $array['photo'] = $file[1];
+        } else {
+            $array['error'] = $validator->errors()->first();
+        }
+        
+        return $array;
+    }
+    
+    public function setOpeningServiceOrder(Request $request) {
+        $array = ['error' => ''];
+        $validator = Validator::make($request->all(), [
+            'title' => 'required',
+            'property' => 'required'
+        ]);
+        if (!$validator->fails()) {
+            $title = $request->input('title');
+            $property = $request->input('property');
+            $list = $request->input('list');
+            $newOpeningServiceOrder = new OpeningServiceOrder();
+            $newOpeningServiceOrder->id_unit = $property;
+            $newOpeningServiceOrder->title = $title;
+            $newOpeningServiceOrder->status = 'IN_REVIEW';
+            $newOpeningServiceOrder->datecreated = date('Y-m-d');
+            $newOpeningServiceOrder->save();
+            if ($list && is_array($list)) {
+                foreach ($list as $listItem) {
+                    $newOpeningServiceOrderItem = new OpeningServiceOrderItem();
+                    $newOpeningServiceOrderItem->id_opening_service_order = $newOpeningServiceOrder->id;
+                    $newOpeningServiceOrderItem->description = $listItem['description'];
+                    $newOpeningServiceOrderItem->status = 'IN_REVIEW';
+                    $newOpeningServiceOrderItem->save();
+                    if (isset($listItem['photos']) && is_array($listItem['photos'])) {
+                        $photos = [];
+                        foreach ($listItem['photos'] as $photo) {
+                            $url = explode('storage/', $photo);
+                            $photos[] = $url[1];
+                        }
+                        $newOpeningServiceOrderItem->photos = implode(',', $photos);
+                        $newOpeningServiceOrderItem->save();
+                    }
+                }
+            }
+        } else {
+            $array['error'] = $validator->errors()->first();
+        }
+        return $array;
+    }
+
 }
